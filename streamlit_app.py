c4530302af5790a164dbd0b55729318006726c35
NAVEEN RAJ KS <109338262+Naveenraj295@users.noreply.github.com>
2022-07-16 14:34:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Fruit Smoothies 🥝🍇')
-streamlit.text('🍌 Bannana Shake')
-streamlit.text('🥭 Mango Delite')
-streamlit.text('🥝 Kewi Crushers')
-streamlit.text('🍇 Grape Pulpes')
 
 
 import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
